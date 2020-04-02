529630d1afd530c1bfb13e00822282576cb6e75c
Othon-Code <othonn.ssousa@gmail.com>
2020-04-01 22:44:04 -0300

    Primeira classe criada: Posicao

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,10 +1,13 @@
 package application;
 
+import tabjogo.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 
-		System.out.println("Olá galera !!");
+		Posicao p1 = new Posicao(3, 5);
+		System.out.println(p1);
 	}
 
 }
