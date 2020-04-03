6089d4fe4a9a470c7eb50c66880d88ba03bb4f35
Othon-Code <othonn.ssousa@gmail.com>
2020-04-02 23:40:14 -0300

    TabException e programação defensiva

---
+++ src/tabjogo/TabException.java
@@ -0,0 +1,12 @@
+package tabjogo;
+
+public class TabException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public TabException(String msg) {
+		super(msg);
+
+	}
+
+}
--- src/tabjogo/Tabuleiro.java
+++ src/tabjogo/Tabuleiro.java
@@ -8,6 +8,10 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int qtdeLinhas, int qtdeColunas) {
+		if (qtdeLinhas < 1 || qtdeColunas < 1) {
+			throw new TabException(
+					"Erro na criacao do tabuleiro: é nescessário que exista ao menos 1 linha e 1 coluna");
+		}
 		this.qtdeLinhas = qtdeLinhas;
 		this.qtdeColunas = qtdeColunas;
 		pecas = new Peca[qtdeLinhas][qtdeColunas];
@@ -17,29 +21,46 @@ public class Tabuleiro {
 		return qtdeLinhas;
 	}
 
-	public void setQtdeLinhas(int qtdeLinhas) {
-		this.qtdeLinhas = qtdeLinhas;
-	}
-
 	public int getQtdeColunas() {
 		return qtdeColunas;
 	}
 
-	public void setQtdeColunas(int qtdeColunas) {
-		this.qtdeColunas = qtdeColunas;
-	}
-
 	public Peca peca(int lin, int col) {
+		if (!existePosicao(lin, col)) {
+			throw new TabException("Posição não existente no tabuleiro");
+		}
 		return pecas[lin][col];
 	}
 
 	public Peca peca(Posicao posic) {
+		if (!existePosicao(posic)) {
+			throw new TabException("Posição não existente no tabuleiro");
+		}
 		return pecas[posic.getLinha()][posic.getColuna()];
 	}
 
 	public void lugarPeca(Peca peca, Posicao posic) {
+		if (haPeca(posic)) {
+			throw new TabException("Já existe uma peça nesta posição " + posic);
+		}
 		pecas[posic.getLinha()][posic.getColuna()] = peca;
 		peca.posicao = posic;
+	}
+
+	private boolean existePosicao(int lin, int col) {
+		return lin >= 0 && lin < qtdeLinhas && col >= 0 && col < qtdeColunas;
+	}
+
+	public boolean existePosicao(Posicao posic) {
+		return existePosicao(posic.getLinha(), posic.getColuna());
+
+	}
+
+	public boolean haPeca(Posicao posic) {
+		if (!existePosicao(posic)) {
+			throw new TabException("Posição não existente no tabuleiro");
+		}
+		return peca(posic) != null;
 	}
 
 }
